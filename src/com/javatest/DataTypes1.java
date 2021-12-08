0322850cc0f7ae3ea8e6fe44b64040c23056eeee
Rigelsoftware <rigelsoftwaresolutions@gmail.com>
2021-12-08 14:16:42 +0530

    Added Sample java progran

---
+++ src/com/javatest/DataTypes1.java
@@ -0,0 +1,23 @@
+package com.javatest;
+
+public class DataTypes1 {
+
+	public static void main(String s[]) {
+
+		int k = 10;
+		float f = 20;
+		double h = 10.55;
+		char a = 'h';
+		short l = 10;
+		long k1 = 20L;
+	
+		System.out.println("All the primitive data types defined here");
+		System.out.println("Integer "+ k);
+		System.out.println("Float:"+ f);
+		System.out.println("Double:"+ h);
+		System.out.println("char"+a);
+		System.out.println("Short"+ l);
+		System.out.println("K1"+ k1);
+	}
+
+}
---
+++ src/com/javatest/DataTypes2.java
@@ -0,0 +1,15 @@
+package com.javatest;
+
+public class DataTypes2 {
+
+	public static void main(String[] args) {
+
+		Integer a = 10;
+		Float b = 11f;
+		Long longv = 10L;
+		String astr = "aaquib";
+		Double d = 10.5;
+
+	}
+
+}
